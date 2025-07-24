24b4df6e2a3204cea0a000a5cb9b809f835adffa
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-23 21:17:26 -0300

    feat: transaction route plugin

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'New transaction',
-      amount: 5000,
-    })
-    .returning('*')
-
-  return transactions
-})
-
-app.get('/t', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
-})
+app.register(transactionsRoutes)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server is running on port ${env.PORT}`)
