4e11b5f72fa12d4324aeb27f15875cd349b143c8
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-07-24 22:28:30 -0300

    feat: tests w/ supertest & vitest

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,52 @@
+import { it, expect, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+
+import { app } from '../app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.status).toBe(201)
+  })
+
+  it.only('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('Cookie not found')
+    }
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.status).toBe(200)
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+})
