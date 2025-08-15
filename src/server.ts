341398e21b7b5784f29f72e0355ca1805a6af5f3
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-14 23:53:06 -0300

    feat: port change

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT || 4000 }).then(() => {
   console.log(`Server is running on port ${env.PORT}`)
 })
