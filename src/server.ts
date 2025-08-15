7149049d2f017898e09518198ca728eefd930782
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-08-15 09:56:44 -0300

    feat: verifying host

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT || 4000 }).then(() => {
-  console.log(`Server is running on port ${env.PORT}`)
-})
+app
+  .listen({
+    port: env.PORT || 4000,
+    host: env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0',
+  })
+  .then(() => {
+    console.log(`Server is running on port ${env.PORT}`)
+  })
