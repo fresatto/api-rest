6a8b83ba173ab28d6f269e4aab2589bfd3632412
Gabriel Fresatto <gabrielfresatto@gmail.com>
2025-09-01 23:19:37 -0300

    feat: refatoração das funções de data para melhor manipulação de timezone

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,15 +1,18 @@
 import { addHours, startOfDay } from 'date-fns'
-import { toZonedTime } from 'date-fns-tz'
+import { fromZonedTime, toZonedTime } from 'date-fns-tz'
 
 export const DB_DATE_FORMAT = 'yyyy-MM-dd'
 
+export const getCurrentDateInTimezone = (timezone: string) => {
+  return toZonedTime(new Date(), timezone)
+}
+
 export const getCurrentDateInitialAndEndDateInTimezone = (timezone: string) => {
-  const currentDay = toZonedTime(new Date(), timezone)
+  const currentDay = getCurrentDateInTimezone(timezone)
+  const startOfCurrentDay = startOfDay(currentDay)
 
-  const utcInitialDate = startOfDay(currentDay)
-  const utcEndDate = addHours(utcInitialDate, 24)
-
-  console.log({ currentDay, utcInitialDate, utcEndDate })
+  const utcInitialDate = fromZonedTime(startOfCurrentDay, timezone)
+  const utcEndDate = fromZonedTime(addHours(startOfCurrentDay, 24), timezone)
 
   return { utcInitialDate, utcEndDate }
 }
